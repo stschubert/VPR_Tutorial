9688cc0befc2c1b2450cce06c26cde656172d1c2
Stefan Schubert <firstname.lastname@etit.tu-chemnitz.de>
2022-04-28 16:01:07 +0200

    Clean up code

--- evaluation/createPR.py
+++ evaluation/createPR.py
@@ -1,20 +1,20 @@
 #   =====================================================================
 #   Copyright (C) 2022  Stefan Schubert, stefan.schubert@etit.tu-chemnitz.de
-#   
+#
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
-#   
+#
 #   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
-#   
+#
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   =====================================================================
-# 
+#
 import numpy as np
 
 
@@ -43,28 +43,3 @@ def createPR(S, GThard, GTsoft):
         R.append(TP / (TP + FN))  # recall
 
     return P, R
-
-
-##### MAIN FOR PLACE RECOGNITION #####
-if __name__ == '__main__':
-    from scipy.io import loadmat
-    from matplotlib import pyplot as plt
-    plt.ion()
-
-    # ground truth
-    GT = np.eye(288)
-
-    # load descriptors
-    D_summer = loadmat('summer.mat')['D']
-    D_winter = loadmat('winter.mat')['D']
-
-    # compute distance matrix with cosine distance
-    DD = 1 - np.matmul(D_summer, D_winter.transpose())
-
-    # compute precision recall
-    P, R = createPR(DD, np.eye(288))
-
-    # vis
-    print('avgP = ', np.trapz(P, R))
-    plt.plot(R, P)
-    plt.draw()
