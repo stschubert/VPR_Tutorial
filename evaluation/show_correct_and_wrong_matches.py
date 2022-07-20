42992f8dfeee476832b05a2b04b8a34dda7e7a20
Stefan Schubert <firstname.lastname@etit.tu-chemnitz.de>
2022-07-20 11:27:25 +0200

    -change color of the frame for wrong and correct image matches

--- evaluation/show_correct_and_wrong_matches.py
+++ evaluation/show_correct_and_wrong_matches.py
@@ -6,7 +6,7 @@ from skimage.transform import resize
 def add_frame(img_in, color):
     img = img_in.copy()
 
-    w = int(np.round(0.02*img.shape[1]))
+    w = int(np.round(0.01*img.shape[1]))
 
     # pad left-right
     pad_lr = np.tile(np.uint8(color).reshape(1, 1, 3), (img.shape[0], w, 1))
@@ -30,7 +30,7 @@ def show(db_imgs, q_imgs, TP, FP, M=None):
         q_tp = resize(q_tp.copy(), db_tp.shape, anti_aliasing=True)
         q_tp = np.uint8(q_tp*255)
 
-    img = add_frame(np.concatenate([db_tp, q_tp], axis=1), [0, 255, 0])
+    img = add_frame(np.concatenate([db_tp, q_tp], axis=1), [119, 172, 48])
 
     # false positive FP
     try:
@@ -43,7 +43,8 @@ def show(db_imgs, q_imgs, TP, FP, M=None):
             q_fp = resize(q_fp.copy(), db_fp.shape, anti_aliasing=True)
             q_fp = np.uint8(q_fp*255)
 
-        img_fp = add_frame(np.concatenate([db_fp, q_fp], axis=1), [255, 0, 0])
+        img_fp = add_frame(np.concatenate(
+            [db_fp, q_fp], axis=1), [162, 20, 47])
         img = np.concatenate([img, img_fp], axis=0)
     except:
         pass
